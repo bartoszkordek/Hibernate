504142bc59682932e73a54bbe5d11bca0b8a20c3
bartoszkordek <bartosz.kordek10@gmail.com>
2021-01-05 20:27:22 +0100

    JPA Table Per Clase inheritance strategy

--- JPA/src/Company.java
+++ JPA/src/Company.java
@@ -1,7 +1,7 @@
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
